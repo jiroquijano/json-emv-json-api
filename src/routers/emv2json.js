9ca6ba1adbd461751688f154e75bdea736fb4a8a
jiroquijano <jiroquijano@gmail.com>
2020-10-29 16:54:49 +0800

    implemented crc validation functionality and added test to test

--- src/routers/emv2json.js
+++ src/routers/emv2json.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const EMVParser = require('../lib/EMVParser');
 const router = new express.Router();
+const {crc16ccitt} = require('crc');
+const _ = require('lodash');
+
+const checkCRC = (emvinput)=>{
+    const payload = emvinput.slice(0,emvinput.length-8);
+    const crcpart = emvinput.slice(emvinput.length-8);
+    if(crcpart.slice(0,4) !== '6304') return {correct: true}; //if emv string doesn't have crc tag   
+    const calculatedCRC = _.padStart(crc16ccitt(`${payload}6304`).toString(16).toUpperCase(),4,'0');
+    const inputCRC = crcpart.slice(4);
+    if(calculatedCRC === inputCRC) {
+        return {correct: true};
+    }else{
+        return {
+            correct: false,
+            calculatedCRC,
+            inputCRC
+        }
+    }
+}
 
 router.post('/emv2json',(req,res)=>{
     if(typeof(req.body.emvinput) !== 'string') return res.status(400).send({error:'malformed emv input'})
     const emvParser = new EMVParser(req.body.emvinput);
     const result = emvParser.getObjectEquivalent();
     if(result.error) return res.status(400).send({error: `double check emv string characters enclosed within brackets[]: '${result.error}'`});
+    const crcCheckResult = checkCRC(req.body.emvinput);
+    if(crcCheckResult.correct === false) return res.status(400).send({
+        error: `CRC in input EMV is incorrect`,
+        inputCRC: crcCheckResult.inputCRC,
+        calculatedCRC: crcCheckResult.calculatedCRC,
+        translated: result
+    });
     res.send({
         result
     });
--- test/routers/emv2json.test.js
+++ test/routers/emv2json.test.js
@@ -32,8 +32,7 @@ test("Should be able to properly decode EMV string via post to emv2json endpoint
 
 test("Should be able to point out which part of the input EMV string is erratic", async()=>{
     const malformedEMV = '000201hi0211';
-    const response = await request(app)
-        .post('/emv2json')
+    const response = await request(app).post('/emv2json')
         .expect(400)
         .send({
         emvinput: malformedEMV
@@ -41,4 +40,37 @@ test("Should be able to point out which part of the input EMV string is erratic"
     expect(response.body).toEqual({
         error: `double check emv string characters enclosed within brackets[]: '000201[hi]0211'`
     })
+});
+
+test("Should be able to reject input EMV string if crc is not correct", async()=>{
+    const testInputEMV = '00020101021128500011pa.hil.pi020111PBTMNPHIXXX030989230765305031235204601653036085802PH5910MYFOODHALL6011MANDALUYONG624105253CF64D20941AAEFCE8C263A7A070800000000630485A6'
+    const response = await request(app).post('/emv2json')
+        .expect(400).send({
+            emvinput: testInputEMV
+        });
+    expect(response.body).toEqual({
+        error: 'CRC in input EMV is incorrect',
+        inputCRC: '85A6',
+        calculatedCRC: 'D458',
+        translated: {
+            pfi: '01',
+            pim: '11',
+            mait:{
+                guid: 'pa.hil.pi02',
+                acqid: 'PBTMNPHIXXX',
+                merid: '892307653',
+                pnflags: '123' 
+            },
+            mcc: '6016',
+            txCurrency: '608',
+            cc: 'PH',
+            merCity: 'MANDALUYONG',
+            merName: 'MYFOODHALL',
+            additional:{
+                refLabel: '3CF64D20941AAEFCE8C263A7A',
+                termLabel: '00000000'
+            },
+            crc: "85A6"
+        }
+    });
 });
